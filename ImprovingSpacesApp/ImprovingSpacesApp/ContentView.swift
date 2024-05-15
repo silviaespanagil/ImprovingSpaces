19a7a05eeed37cc1f2edc1949e8f0446b6f3c852
Silvia España <silviaespanagil@gmail.com>
2024-05-15 17:04:27 +0200

    Set up new project

---
+++ ImprovingSpacesApp/ImprovingSpacesApp/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  ImprovingSpacesApp
+//
+//  Created by Silvia España Gil on 15/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+            MapView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ImprovingSpacesApp/ImprovingSpacesApp/ImprovingSpacesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImprovingSpacesAppApp.swift
+//  ImprovingSpacesApp
+//
+//  Created by Silvia España Gil on 15/5/24.
+//
+
+import SwiftUI
+
+@main
+struct ImprovingSpacesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
