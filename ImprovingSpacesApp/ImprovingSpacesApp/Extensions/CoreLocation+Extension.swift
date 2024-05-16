36b3968cda78cfc4719cd2252f53a55e556794da
Silvia España <silviaespanagil@gmail.com>
2024-05-16 14:17:12 +0200

    Add extension folder and files

---
+++ ImprovingSpacesApp/ImprovingSpacesApp/Extensions/CoreLocation+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  CoreLocation+Extension.swift
+//  ImprovingSpacesApp
+//
+//  Created by Silvia España Gil on 16/5/24.
+//
+
+import MapKit
+
+extension CLLocationCoordinate2D {
+    
+    static let spain: Self = .init(
+        latitude: 40.4637,
+        longitude: -3.7492
+    )
+}
