092bb93e8d05f8891d1507766eb78ef0009fe4ba
Silvia España <silviaespanagil@gmail.com>
2024-05-22 15:40:44 +0200

    Add methods to allow searching address Conform viewModel to MKLocalSearchCompleterDelegate

--- ImprovingSpacesApp/ImprovingSpacesApp/MapView/MapViewModel.swift
+++ ImprovingSpacesApp/ImprovingSpacesApp/MapView/MapViewModel.swift
@@ -9,18 +9,36 @@ import Foundation
 import MapKit
 import SwiftUI
 
-class MapViewModel: ObservableObject {
+class MapViewModel: NSObject, ObservableObject, MKLocalSearchCompleterDelegate {
     
     @Published var locationPermission: LocationPermissionManager = LocationPermissionManager()
     
     @Published var userLocation: CLLocationCoordinate2D?
     @Published var selectedCoordinate: CLLocationCoordinate2D?
     
+    @Published var searchResults: [MKLocalSearchCompletion] = []
     @Published var mapCameraPosition: MapCameraPosition = .userLocation(
         fallback: .camera(
             MapCamera(centerCoordinate: .spain, distance: 1000)
         )
     )
+    
+    @Published var address: String = ""
+    
+    private var searchCompleter = MKLocalSearchCompleter()
+    
+    override init() {
+        
+        super.init()
+        configureSearchCompleter()
+    }
+    
+    // MARK: - Configuration Methods
+    
+    private func configureSearchCompleter() {
+        searchCompleter.delegate = self
+        searchCompleter.resultTypes = .address
+    }
     
     // MARK: - Update methods
     
@@ -40,4 +58,45 @@ class MapViewModel: ObservableObject {
     func requestLocationPermission() {
         locationPermission.requestLocationPermission()
     }
+    
+    // MARK: - Local Search methods
+    func updateSearchQuery(_ query: String) {
+        
+        address = query
+        searchCompleter.queryFragment = query
+    }
+    
+    func search(_ completion: MKLocalSearchCompletion) {
+        
+        let searchRequest = MKLocalSearch.Request(completion: completion)
+        let search = MKLocalSearch(request: searchRequest)
+        
+        search.start { [weak self] response, error in
+            guard let self = self, let response = response, let item = response.mapItems.first else {
+                print("Search error: \(String(describing: error?.localizedDescription))")
+                return
+            }
+            
+            self.handleSearchResponse(item)
+        }
+    }
+    
+    private func handleSearchResponse(_ item: MKMapItem) {
+        
+        let coordinate = item.placemark.coordinate
+        
+        selectedCoordinate = coordinate
+        mapCameraPosition = .camera(
+            MapCamera(centerCoordinate: coordinate, distance: 1000)
+        )
+    }
+    
+    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+        searchResults = completer.results
+    }
+    
+    func completer(_ completer: MKLocalSearchCompleter, didFailWithError error: Error) {
+        print("Autocomplete error: \(error.localizedDescription)")
+    }
 }
+
