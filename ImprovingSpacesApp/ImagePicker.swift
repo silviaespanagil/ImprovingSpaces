638928916392c893f732c57823a8dffa0d77cff0
Silvia España <silviaespanagil@gmail.com>
2024-05-22 16:12:08 +0200

    Create ImagePicker

---
+++ ImprovingSpacesApp/ImagePicker.swift
@@ -0,0 +1,53 @@
+//
+//  ImagePicker.swift
+//  ImprovingSpacesApp
+//
+//  Created by Silvia España Gil on 22/5/24.
+//
+
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    
+    @Binding var selectedImage: Image?
+    
+    @Environment(\.presentationMode) var presentationMode
+    
+    var sourceType: UIImagePickerController.SourceType = .photoLibrary
+    
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        picker.sourceType = sourceType
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(parent: self)
+        
+    }
+    
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        let parent: ImagePicker
+        
+        init(parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            
+            if let image = info[.originalImage] as? UIImage {
+                parent.selectedImage = Image(uiImage: image)
+            }
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+        
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+}
